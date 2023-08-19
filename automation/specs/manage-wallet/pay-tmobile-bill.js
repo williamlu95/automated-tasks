aecc48fc189c338e36107583e4b1a101f198cdb0
William Lu <williamlu95@gmail.com>
2023-08-19 13:55:42 -0700

    Avoid logging in unnecessarily

--- automation/specs/manage-wallet/pay-tmobile-bill.js
+++ automation/specs/manage-wallet/pay-tmobile-bill.js
@@ -2,10 +2,26 @@ import { TEMPLATE_TRANSACTION } from '../../constants/transaction';
 import TmobileDashboardPage from '../../pageobjects/tmobile-dashboard-page';
 import TmobileLoginPage from '../../pageobjects/tmobile-login-page';
 
+const hasNoBalance = (transactionCountKey) => {
+  const isAlreadyPaid = global.transactionCounts[transactionCountKey] > 0;
+
+  if (isAlreadyPaid) {
+    return true;
+  }
+
+  const isBeforeBillDate = new Date().getDate() < 25;
+
+  if (isBeforeBillDate) {
+    return true;
+  }
+
+  return false;
+};
+
 export const payTmobileBill = async () => {
   const { transactionCountKey, ...restOfTemplate } = TEMPLATE_TRANSACTION.TMOBILE;
 
-  if (global.transactionCounts[transactionCountKey] > 0) {
+  if (hasNoBalance(transactionCountKey)) {
     return [];
   }
 
