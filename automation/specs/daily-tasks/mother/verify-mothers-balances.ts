a0aed098d2c8b673bb15bf9ab77fd5e77f7dcdb0
William Lu <52891447+williamlu95@users.noreply.github.com>
2023-12-23 11:00:35 -0800

    Update verify-mothers-balances.ts

--- automation/specs/daily-tasks/mother/verify-mothers-balances.ts
+++ automation/specs/daily-tasks/mother/verify-mothers-balances.ts
@@ -5,9 +5,9 @@ import { MothersTransactions } from './mothers-transactions';
 const RUN_AT_HOUR = 20;
 
 export const verifyMothersBalances = async () => {
-  // if (RUN_AT_HOUR !== new Date().getHours()) {
-  //   return;
-  // }
+  if (RUN_AT_HOUR !== new Date().getHours()) {
+    return;
+  }
 
   readMothersEmails(false);
 
