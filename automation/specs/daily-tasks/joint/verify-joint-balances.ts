fa9c0e8850ef47718ef7d60aa4f33f2c46be2505
William Lu <52891447+williamlu95@users.noreply.github.com>
2023-12-23 10:49:06 -0800

    Update verify-joint-balances.ts

--- automation/specs/daily-tasks/joint/verify-joint-balances.ts
+++ automation/specs/daily-tasks/joint/verify-joint-balances.ts
@@ -2,7 +2,7 @@ import GoogleBalanceSheetPage from '../../../pageobjects/google-balance-sheet-pa
 import { readJointEmails } from '../../../utils/notification';
 import { JointTransactions } from './joint-transactions';
 
-const RUN_AT_HOUR = new Date().getHours();
+const RUN_AT_HOUR = 20;
 
 export const verifyJointBalances = async () => {
   if (RUN_AT_HOUR !== new Date().getHours()) {
