6fe9fe2a4a1a8e3db4af0c793cfb7af7311abdb1
William Lu <52891447+williamlu95@users.noreply.github.com>
2024-02-25 16:33:20 -0800

    Update mother-daily-tasks.e2e.ts

--- automation/specs/daily-tasks/mother/mother-daily-tasks.e2e.ts
+++ automation/specs/daily-tasks/mother/mother-daily-tasks.e2e.ts
@@ -1,6 +1,6 @@
 import { verifyMothersBalances } from './verify-mothers-balances';
 
-describe('Mother daily tasks', () => {
+describe.skip('Mother daily tasks', () => {
   before(async () => {
     await verifyMothersBalances();
   });
