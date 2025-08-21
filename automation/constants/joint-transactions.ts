047eea533bae5443b4939c2c7aaa6ab38671196a
William Lu <52891447+williamlu95@users.noreply.github.com>
2025-08-21 14:31:40 -0700

    Update joint-transactions.ts

--- automation/constants/joint-transactions.ts
+++ automation/constants/joint-transactions.ts
@@ -94,7 +94,7 @@ export const generateExpenseForDate = (date: DateTime): Record<string, ExpectedT
   ELECTRICITY: {
     identifier: 'Electric',
     name: 'Nv Energy',
-    amount: 142.0,
+    amount: 118.0,
     day: 10,
     type: TRANSACTION_TYPE.EXPENSE,
   },
