87cec5888e863a31b0ca69ecfcf377658e14bdf6
William Lu <williamlu95@gmail.com>
2025-08-27 19:37:38 -0700

    Comment out code that isn't ready

--- automation/specs/daily-tasks/joint/joint-daily-tasks.ts
+++ automation/specs/daily-tasks/joint/joint-daily-tasks.ts
@@ -1,8 +1,8 @@
 import { readPersonalEmails } from '../../../utils/notification';
 import { JointTransactions } from './joint-transactions';
 import { verifyJointBalances } from './verify-joint-balances';
-import WalletRecordPage from '../../../pageobjects/wallet-record-page';
-import { DailyTaskData } from '../daily-task-data';
+// import WalletRecordPage from '../../../pageobjects/wallet-record-page';
+// import { DailyTaskData } from '../daily-task-data';
 
 export const runJointDailyTasks = async () => {
   readPersonalEmails(false);
@@ -10,19 +10,19 @@ export const runJointDailyTasks = async () => {
   const transactions = new JointTransactions();
   transactions.initializeTransactions();
 
-  const pendingTransactions = await WalletRecordPage.getPendingTransactions();
+  // TODO: add back in once script is updated
+  // const pendingTransactions = await WalletRecordPage.getPendingTransactions();
 
-  for (let i = pendingTransactions.length - 1; i >= 0; i -= 1) {
-    const pendingTransaction = pendingTransactions[i];
-    const isPresent = transactions.isPresent(pendingTransaction.date, pendingTransaction.amount);
+  // for (let i = pendingTransactions.length - 1; i >= 0; i -= 1) {
+  //   const pendingTransaction = pendingTransactions[i];
+  //   const isPresent = transactions.isPresent(pendingTransaction.date, pendingTransaction.amount);
 
-    if (isPresent) {
-      await WalletRecordPage.removeRecord(i);
-    }
-  }
+  //   if (isPresent) {
+  //     await WalletRecordPage.removeRecord(i);
+  //   }
+  // }
 
-  await DailyTaskData.refreshExpectedBalance();
-  transactions.initializeTransactions();
-
+  // await DailyTaskData.refreshExpectedBalance();
+  // transactions.initializeTransactions();
   await verifyJointBalances(transactions.getBalanceSheet());
 };
