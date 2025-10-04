22cd46b35302bb78ac2c900825b8bc3b261fa8ba
William Lu <52891447+williamlu95@users.noreply.github.com>
2025-10-04 10:17:36 -0700

    Update currency-formatter.ts

--- automation/utils/currency-formatter.ts
+++ automation/utils/currency-formatter.ts
@@ -1,3 +1,3 @@
 export const formatToDollars = (amount: number | string): string => new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(Number(amount || 0));
 
-export const formatFromDollars = (dollars: string): number => Number(dollars?.replace(/[$,]+/g, ''));
+export const formatFromDollars = (dollars: string): number => Number(dollars?.replace(/[$,]+/g, '') || 0);
